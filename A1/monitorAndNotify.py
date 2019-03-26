a218cb5a9b96135a5d9bbca0bcab9f91e365d340
Andrew <s3559384@student.rmit.edu.au>
2019-03-26 12:46:11 +1100

    monitor&notify

--- A1/monitorAndNotify.py
+++ A1/monitorAndNotify.py
@@ -3,25 +3,19 @@ from virtual_sense_hat import VirtualSenseHat
  
 def getCurrentTime():
     sense = VirtualSenseHat.getSenseHat()
-    getTime = datetime.datetime.now().strftime('%I:%M %p')
-    sense.show_message('Time: ', getTime)
-    return getTime
-    print(getTime)
-
+    timestamp = datetime.datetime.now().strftime('%I:%M %p')
+    sense.show_message('Time: %s' % timestamp)
 getCurrentTime()
-# OR
-# sense = VirtualSenseHat.getSenseHat(False)
+
 def getWeatherTemp(): 
    sense = VirtualSenseHat.getSenseHat()
    temperature = sense.get_temperature()
    sense.show_message('Temperature: {0:0.1f} *C'.format(temperature))
-   return temperature
-
 getWeatherTemp()
+
 def getWeatherHumid():
     sense = VirtualSenseHat.getSenseHat()
     humidity = sense.get_humidity()
     sense.show_message('Humidity: {0:0.0f}%'.format(humidity))
     return humidity
-
 getWeatherHumid()

897832b8df6cb3851187ecbf2d686f9ef4cff09c
rmit-s3559384-andrew-alvaro <s3559384@student.rmit.edu.co.aum>
2019-03-27 11:50:01 +1100

    update

--- A1/create_db.py
+++ A1/create_db.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python3
+
 import sqlite3 as lite
 import sys
 
-con = lite.connect("report.db")
+class CreateDB:
+	
+	def createData():
+		connection = sqlite3.connect("sensehat.db")
 
-with con:
-    cur = con.cursor()
-    cur.execute()
+		with connection:
+			connection.execute("DROP TABLE IF EXISTS sensehat_data")
+			connection.execute("CREATE TABLE sensehat_data(timestamp DATETIME, temperature NUMERIC. humidity NUMERIC)")
+		connection.close()
+
+	createData()	
